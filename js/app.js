adbe5488f29af14697dc529fc60a70957bf9c848
Lyaysan35 <lyaysanglick@gmail.com>
2019-10-06 16:13:10 -0600

    label and radio input

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 const questions = [
 	{ q: "How many green elements in the picture?", a: 2, c: [1, 2, 3, 4] },
 	{ q: "Which color is car?", a: 0 , c: ["yellow", "red", "green", "blue"] },
-	{ q: "How many blue elements in the picture", a: 2, c: [3, 2, 1, 5] },
+	{ q: "How many blue elements in the picture?", a: 2, c: [3, 2, 1, 5] },
 	{ q: "How many food items in the picture?", a: 3, c: [8,1,3,5] },
-	{ q: "In which row on the bird", a: 0 , c: [2,1,4,3] }
+	{ q: "In which row did the bird appear?", a: 0 , c: [2,1,4,3] }
 ];
 
 
@@ -18,25 +18,47 @@ function startGame () {
 }
 
 function setTimer() {
-	const interval = setInterval(() => {
+	const interval = setTimeout(() => {
 		document.querySelector('.quest-pic1').style.display = 'none';
-		const form = document.createElement('div');
-		form.id = 'questions';
-		document.querySelector('body').appendChild(form);
-		displayQuestion(form);
+		const div = document.createElement('div');
+		div.id = 'questions';
+		document.querySelector('body').appendChild(div);
+		displayQuestion(div);
 		clearInterval(interval);
 	}, 1000 * 5);
 }
 
-function displayQuestion(form) {
+function displayQuestion(div) {
 	const prompt = questions[Math.floor(Math.random() * questions.length)];
 	const question = prompt.q;
 	const choices = prompt.c;
 
-	const choice1 = document.createElement('input');
-	choice1.type = 'radio';
-	choice1.textContent = choices[0];
-	form.appendChild(choice1);
+	const label1 = document.createElement('label');
+	label1.textContent = prompt.q;
+	div.appendChild(label1);
+
+	for(let i = 0; i < prompt.c.length; i++) {
+		const section = document.createElement('section');
+		div.appendChild(section);
+
+		// Add radio button to select choice
+		let select = document.createElement('input');
+		select.type = 'radio';
+		select.id = i;
+		select.className = 'option';
+
+		// Append radio button to form div
+		section.appendChild(select);
+
+		// Create text description of choice
+		let choice = document.createElement('label');
+		choice.textContent = prompt.c[i];
+
+		// Append text description to form div
+		section.appendChild(choice);
+		choice.className = 'option';
+	}
+
 
 }
 
@@ -64,6 +86,7 @@ class guessPict {
 		image.style.width = "50%";
 		image.style.height = "50%";
 		document.querySelector("#pict").appendChild(image);
+		document.querySelector("h1").style.color = "blue";
 
 	}
 }
